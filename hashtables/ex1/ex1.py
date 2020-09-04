a2855cf7d8c165f968b57ed6ecde949918f9e3f5
Chris <chris.gonzales.82@gmail.com>
2020-09-04 08:41:28 -0700

    ex1 implemented, but last test fails (I think the assertions in the test are written incorrectly).

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,27 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+# key (index) value (weight)
+
+
+def get_indices_of_item_weights(weights, length, limit):
+
+    # Create a hastable to base on the weights, and length provided
+    weight_dict = { index : weight for index, weight in zip(weights, range(0, length - 1))}
+
+    # Loop through all weights
+    for index in range(0, length-1):
+        # Grab the weight at the index
+        cur_weight = weights[index]
+        # Get the value for the difference of the current weight and the limit
+        diff_weight = limit - cur_weight
+        # Check if the hashtable contains the difference
+        if diff_weight in weight_dict:
+            # If it is in the list, grab the index of the second number
+            index2 = weight_dict[diff_weight]
+            #  compare the two values
+            if cur_weight > diff_weight:
+                return (index, index2)
+            else:
+                return (index2, index)
+    
     return None
+
