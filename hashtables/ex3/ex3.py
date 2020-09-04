a256066054bed2201d3645ad6003451321048379
Chris <chris.gonzales.82@gmail.com>
2020-09-04 09:40:12 -0700

    ex3 implemented. All test pass

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,9 +1,28 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+  # Find the intersection between multiple lists of integers.
+    # Create a hashtable for all the values present
+    comparison_dict = {}
+    # Create storage for the return results
+    result = []
 
+    # Loop through all the arrays
+    for array in arrays:
+        # Loop through each index in the arrays
+        for value in array:
+            # Check if the value at the index is in the hashtable
+            if value not in comparison_dict:
+                # If not, add it, and increment the count
+                comparison_dict[value] = 1
+            else:
+                # If the value is in the hashtable, add to the intersection list
+                if value in result:
+                    continue
+                else:
+                    result.append(value)
+                
+    
+                
+    # Return the results
     return result
 
 
